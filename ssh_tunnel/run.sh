8b5320f56a9d3db9a65001e613f50b5562b7c001
Alexey Khit <alexey.khit@gmail.com>
2020-04-21 18:44:13 +0300

    Connection recovery

--- ssh_tunnel/run.sh
+++ ssh_tunnel/run.sh
@@ -7,33 +7,52 @@ bashio::log.info "Public key:"
 
 cat /root/.ssh/id_rsa.pub
 
-CMD='-o StrictHostKeyChecking=no -N'
+BASE="ssh -o StrictHostKeyChecking=no"
+TUN="-o ExitOnForwardFailure=yes -o ServerAliveInterval=30 -N"
 
 if bashio::config.true 'tunnel_http'; then
-    CMD="${CMD} -R 80:localhost:80"
+    # -R remote_socket:host:hostport
+    # Specifies that connections to the given TCP port or Unix socket on the
+    # remote (server) host are to be forwarded to the local side.
+    TUN="${TUN} -R 80:localhost:80"
 fi
 
 if bashio::config.true 'tunnel_https'; then
-    CMD="${CMD} -R 443:localhost:443"
+    TUN="${TUN} -R 443:localhost:443"
 fi
 
 if ! bashio::config.equals 'socks_port' 0; then
-    CMD="${CMD} -D *:$(bashio::config 'socks_port')"
+    # -D [bind_address:]port
+    # Currently the SOCKS4 and SOCKS5 protocols are supported, and ssh will act
+    # as a SOCKS server.
+    TUN="${TUN} -D *:$(bashio::config 'socks_port')"
 fi
 
 if ! bashio::config.is_empty 'advanced'; then
-    CMD="${CMD} $(bashio::config 'advanced')"
+    TUN="${TUN} $(bashio::config 'advanced')"
 fi
 
-if ! bashio::config.equals 'ssh_port' 0; then
-    CMD="${CMD} -p $(bashio::config 'ssh_port')"
+SRV="$(bashio::config 'ssh_user')@$(bashio::config 'ssh_host')"
+
+if ! bashio::config.equals 'ssh_port' 22; then
+    SRV="-p $(bashio::config 'ssh_port') ${SRV}"
 fi
 
-CMD="${CMD} $(bashio::config 'ssh_user')@$(bashio::config 'ssh_host')"
+SRV="-o StrictHostKeyChecking=no ${SRV}"
+
+set +e
 
 while true
 do
-    bashio::log.info "run tunnel: ssh ${CMD}"
-    sh -c "ssh ${CMD}"
-    sleep 1
+    if ! bashio::config.is_empty 'before'; then
+        CMD="${BASE} ${SRV} $(bashio::config 'before')"
+        bashio::log.info "run: ${CMD}"
+        eval $CMD
+    fi
+
+    CMD="${BASE} ${TUN} ${SRV}"
+    bashio::log.info "run tunnel: ${CMD}"
+    eval $CMD
+
+    sleep 30
 done

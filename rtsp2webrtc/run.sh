c3fc53e94aa09fe1a1803b3fd1532b0a84d1cb97
Alexey Khit <alexey.khit@gmail.com>
2020-11-11 09:16:41 +0300

    Add rtsp2webrtc

---
+++ rtsp2webrtc/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+
+cat /data/options.json | jq "
+    {server: {http_port: \":8083\"}, streams: [.streams[] | {(.name): {url: .url}}] | add}
+    " > /app/config.json
+
+cat /app/config.json
+
+/app/RTSPtoWebRTC
